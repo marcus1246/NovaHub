c3e5233179584683d4e6a1cd36b31f3c50caad66
Marcus Bachira <marcusomzcool@gmail.com>
2026-08-01 21:04:08 +0200

    Fix formatting and improve logout redirection

--- dashboard.js
+++ dashboard.js
@@ -4,13 +4,31 @@ let user = localStorage.getItem("loggedInUser");
 
 if(!user){
 
-    window.location.href = "login.html";
+    window.location.href="login.html";
 
 }
 
 
 
+
 document.getElementById("username").innerHTML = user;
+
+
+
+
+let workouts = localStorage.getItem("totalWorkouts");
+
+
+
+if(workouts === null){
+
+    workouts = 0;
+
+}
+
+
+
+document.getElementById("workoutCount").innerHTML = workouts;
 
 
 
@@ -22,7 +40,7 @@ function logout(){
     localStorage.removeItem("loggedInUser");
 
 
-    window.location.href = "login.html";
+    window.location.href="login.html";
 
 
 }
