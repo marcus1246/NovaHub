b2093550de9b7578d02ff86cda7671223474b3db
Marcus Bachira <marcusomzcool@gmail.com>
2026-08-01 20:48:55 +0200

    Fix formatting and structure in dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,21 +1,28 @@
 let user = localStorage.getItem("loggedInUser");
 
 
-if (!user) {
+
+if(!user){
 
     window.location.href = "login.html";
 
 }
 
 
+
 document.getElementById("username").innerHTML = user;
 
 
 
-function logout() {
+
+
+function logout(){
+
 
     localStorage.removeItem("loggedInUser");
 
+
     window.location.href = "login.html";
+
 
 }
